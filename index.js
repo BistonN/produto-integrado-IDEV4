139277d3ad909637a8b3c8d1e179888a32593214
BistonN <biston.nunes@gmail.com>
2025-12-02 09:08:33 -0300

    subindo atividade

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 const prompt = require("prompt-sync")();
 const fs = require('fs');
-const path = require('path');
-
-while (true) {
-    const escolha = prompt(`
-0: Sair
-
-Escolha uma opção do menu
-    `);
-
-    if (escolha == "0") {
-        break;
-    } else {
-        continue;
-    }
-}
 
 function loadDB() {
     try {
@@ -30,39 +15,68 @@ function loadDB() {
         };
     }
 }
+let db = loadDB();
+console.log(db["pontos_bicicleta"][0]["bicicletas"][3]["valor"]) // ex
 
-function saveDB(db) {
-    try {
-        fs.writeFileSync("./bd.json", JSON.stringify(db, null, 4), 'utf8');
-        return true;
-    } catch (err) {
-        console.error('Erro ao salvar bd.json:', err.message);
-        return false;
-    }
-}
+// acessem o nome de primeiro usuario registrado
+// acessem o "inicio" da primeira corrida registrada
+// todas as bicicletas do pronto B
+// tipo da bicicleta id 12
 
-function getNextId(name) {
-    const db = loadDB(); // objeto
 
-    const values = db.name || [];
 
-    let maxId = 0;
-    for (let i = 0; i < usuarios.length; i++) {
-        const u = usuarios[i];
-        if (typeof u.id === 'number' && u.id > maxId) {
-            maxId = u.id;
-        }
-    }
-    const newId = maxId !== 0 ? maxId + 1 : 1; // if one line
-    //condição (true ou false) ? valor se verdadeiro : valor se falso 
 
-    // if (maxId !== 0) {
-    //     const newId = maxId + 1
-    // } else {
-    //      const newId = 1
-    // }
-}
 
-function cadastrarUsuario(nome) {
+
+// function saveDB(db) {
+//     try {
+//         fs.writeFileSync("./bd.json", JSON.stringify(db, null, 4), 'utf8');
+//         return true;
+//     } catch (err) {
+//         console.error('Erro ao salvar bd.json:', err.message);
+//         return false;
+//     }
+// }
+
+
+// while (true) {
+//     const escolha = prompt(`
+// 0: Sair
+
+// Escolha uma opção do menu
+//     `);
+
+//     if (escolha == "0") {
+//         break;
+//     } else {
+//         continue;
+//     }
+// }
+
+
+
+// function getNextId(name) {
+//     const db = loadDB(); // objeto
+
+//     const values = db.name || [];
+
+//     let maxId = 0;
+//     for (let i = 0; i < usuarios.length; i++) {
+//         const u = usuarios[i];
+//         if (typeof u.id === 'number' && u.id > maxId) {
+//             maxId = u.id;
+//         }
+//     }
+//     const newId = maxId !== 0 ? maxId + 1 : 1; // if one line
+//     //condição (true ou false) ? valor se verdadeiro : valor se falso 
+
+//     // if (maxId !== 0) {
+//     //     const newId = maxId + 1
+//     // } else {
+//     //      const newId = 1
+//     // }
+// }
+
+// function cadastrarUsuario(nome) {
     
-}
+// }
